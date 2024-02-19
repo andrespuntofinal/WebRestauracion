013e3678ca47db530a5c4bd1bafdda8ab8fd4fdd
Andrés Jaramillo <andrespuntofinal@gmail.com>
2024-02-19 11:32:37 -0500

    responsive 41

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    myAppUrl:'https://backresta-3oxjukqpga-uc.a.run.app/',
+    myAppUrl:'https://api-restauracion-3oxjukqpga-uc.a.run.app777/',
     urlgoogle:'https://identitytoolkit.googleapis.com/v1/accounts',
     apikeygoogle:'AIzaSyBMwHSn93JmIwv4t7lZ7ofAR_v_A869hAM',
   };
